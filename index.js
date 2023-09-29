42503b8f3f394a031e0de9c663149cb5d5689eeb
julio cesar <jcmcf2012@gmail.com>
2023-09-29 19:58:12 -0300

    style: format code

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
-const swaggerUi = require('swagger-ui-express');
-const swaggerOptions = {customCssUrl: '/swagger-ui.css'};
-const swaggerFile = require('./swagger/swagger_output.json');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerOptions = { customCssUrl: "/swagger-ui.css" };
+const swaggerFile = require("./swagger/swagger_output.json");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 
-const routes = require('./src/routes');
+const routes = require("./src/routes");
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-if(process.env.NODE_ENV !== 'test'){
-    const swaggerFile = require('./swagger/swagger_output.json');
-    app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc'); });
-    app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
+if (process.env.NODE_ENV !== "test") {
+  const swaggerFile = require("./swagger/swagger_output.json");
+  app.get("/", (req, res) => {
+    /* #swagger.ignore = true */ res.redirect("/doc");
+  });
+  app.use(
+    "/doc",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerFile, swaggerOptions)
+  );
 }
-
 
 routes(app);
 
-if (process.env.NODE_ENV !== 'test') {
-    const PORT = process.env.PORT || 4000;
-    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+if (process.env.NODE_ENV !== "test") {
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 }
 
 module.exports = app;
