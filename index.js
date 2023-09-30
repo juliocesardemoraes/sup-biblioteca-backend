cbd8f410da35aff5ccb7690ffe0bd2ea57f5b0c6
julio cesar <jcmcf2012@gmail.com>
2023-09-30 07:50:15 -0300

    fix: testing some files

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+// Importação
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
-const swaggerOption = { customCssUrl: "/swagger-ui.css" };
+const swaggerOptions = { customCssUrl: "/swagger-ui.css" };
 const routes = require("./src/routes");
 const autDocProduction = require("./src/midllewares/autDoch");
 const app = express();
 require("dotenv").config();
 
+// Configuração do Express.js
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
@@ -17,24 +19,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Documentação do Swagger
 if (process.env.NODE_ENV !== "test") {
   const swaggerFile = require("./swagger/swagger_output.json");
   app.get("/", (req, res) => {
-    /* swagger.ignore = true */ res.redirect("/doc");
+    /* #swagger.ignore = true */ res.redirect("/doc");
   });
   app.use(
     "/doc",
-    autDocProduction,
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerFile, swaggerOption)
+    /*authDocProducao*/ swaggerUi.serve,
+    swaggerUi.setup(swaggerFile, swaggerOptions)
   );
 }
 
+// Restante dos endpoints, rotas da API
 routes(app);
 
+// Inicialização do nosso servidor
 if (process.env.NODE_ENV !== "test") {
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 }
 
+// Exportação do módulo
 module.exports = app;
