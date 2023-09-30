a50a849a0965666f52e4f5419daff0e202a7c54b
julio cesar <jcmcf2012@gmail.com>
2023-09-30 07:27:45 -0300

    fix: some small fixes

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
-const swaggerOptions = { customCssUrl: "/swagger-ui.css" };
-const swaggerFile = require("./swagger/swagger_output.json");
+const swaggerOption = { customCssUrl: "/swagger-ui.css" };
+const routes = require("./src/routes");
+const autDocProduction = require("./src/midllewares/autDoch");
 const app = express();
 require("dotenv").config();
-
-const routes = require("./src/routes");
 
 app.use(cors());
 app.use(logger("dev"));
@@ -21,19 +20,20 @@ app.use(express.static(path.join(__dirname, "public")));
 if (process.env.NODE_ENV !== "test") {
   const swaggerFile = require("./swagger/swagger_output.json");
   app.get("/", (req, res) => {
-    /* #swagger.ignore = true */ res.redirect("/doc");
+    /* swagger.ignore = true */ res.redirect("/doc");
   });
   app.use(
     "/doc",
+    autDocProduction,
     swaggerUi.serve,
-    swaggerUi.setup(swaggerFile, swaggerOptions)
+    swaggerUi.setup(swaggerFile, swaggerOption)
   );
 }
 
 routes(app);
 
 if (process.env.NODE_ENV !== "test") {
-  const PORT = process.env.PORT || 4000;
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 }
 
